0cda16fc37b88c9c08fdc35dfff667a30963bf85
ilya-baak <88633824+ilya-baak@users.noreply.github.com>
2023-03-20 14:43:20 -0400

    Updated starter file from name/input to username/login. Need to hash passwords in DB.

--- app.py
+++ app.py
@@ -13,24 +13,27 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/name', methods=['POST', 'GET'])
-def name():
+@app.route('/login', methods=['POST', 'GET'])
+def login():
     error = None
     if request.method == 'POST':
-        result = valid_name(request.form['FirstName'], request.form['LastName'])
+        result = valid_login(request.form['UserName'], request.form['Password'])
         if result:
             return render_template('home.html', error=error, result=result)
         else:
-            error = 'invalid login'
-    return render_template('input.html', error=error)
+            error = 'Invalid login credentials. Please try again.'
+    return render_template('login.html', error=error)
 
 
-def valid_name(first_name, last_name):
+def valid_login(user_name, password_in):
     connection = sql.connect('database.db')
-    cursor = connection.execute(f'SELECT * FROM users WHERE firstname=? AND lastname=?;',(first_name, last_name))
+    cursor = connection.execute(f'SELECT * FROM users WHERE username=? AND password=?;',(user_name, password_in))
     connection.commit()
-    #cursor = connection.execute('SELECT * FROM users;')
     return cursor.fetchall()
+
+def hash_passwords(password):
+    hash = sha256_crypt.hash(password)
+
 
 
 if __name__ == "__main__":
--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 DELETE FROM users WHERE firstname = 'Markus';
 DELETE FROM users WHERE firstname = 'Jay';
+
 DELETE FROM users WHERE firstname = 'email';
+
+ALTER TABLE users RENAME COLUMN firstname TO username;
+ALTER TABLE users RENAME COLUMN lastname TO password;
